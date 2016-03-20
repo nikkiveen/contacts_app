097c9b362187bfb226df3b69f4a6eed2193e31f8
Nikki Veen <nikkiveen@gmail.com>
2016-03-20 12:00:30 -0500

    added devise gem, added test user, added user_id column to contacts table, updated create method to add current_user id when new user added

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+# devise
+gem 'devise'
+# geocoder
+gem 'geocoder'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,11 +9,16 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @coordinates = Geocoder.coordinates(params[:address])
+
     Contact.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-      phone_number: params[:phone_number]
+      phone_number: params[:phone_number],
+      latitude: @coordinates[0],
+      longitude: @coordinates[1]
+      user_id: current_user.id
     )
 
     render 'create.html.erb'
